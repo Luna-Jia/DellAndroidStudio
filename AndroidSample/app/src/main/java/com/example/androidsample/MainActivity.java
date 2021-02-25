ea170fd1c32d139c733fb3d1f278e1373273f65f
Luna Meng Jia <luna.jia6@gmail.com>
2021-02-24 23:03:01 -0500

    Convert the AndroidSample project to [view bindings] instead of findViewById()

--- AndroidSample/app/src/main/java/com/example/androidsample/MainActivity.java
+++ AndroidSample/app/src/main/java/com/example/androidsample/MainActivity.java
@@ -4,31 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+//import android.widget.EditText;
+//import android.widget.TextView;
+import com.example.androidsample.databinding.ActivityMainBinding;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 
     public void convertCurrency(View view) {
 
-        EditText dollarText = findViewById(R.id.dollarText);
-        TextView textView = findViewById(R.id.textView);
+        // EditText dollarText = findViewById(R.id.dollarText);
+        // TextView textView = findViewById(R.id.textView);
 
-        if (!dollarText.getText().toString().equals("")){
+        if (!binding.dollarText.getText().toString().equals("")){
 
-            float dollarValue = Float.valueOf(dollarText.getText().toString());
+            float dollarValue = Float.valueOf(binding.dollarText.getText().toString());
             float euroValue = dollarValue * 0.85F;
-            textView.setText(String.format(Locale.getDefault(), "%.2f",euroValue));
+            binding.textView.setText(String.format(Locale.getDefault(), "%.2f",euroValue));
         } else {
-            textView.setText(R.string.no_value_string);
+            binding.textView.setText(R.string.no_value_string);
         }
     }
 }
