c3d5697326f85b692fe9d7bb766a4eff4956462d
Luna Meng Jia <luna.jia6@gmail.com>
2021-02-25 18:38:43 -0500

    Update StateChange project: Update state with onRestoreInstanceState() method and onCreate() method so that the Activity retains changes through the destruction adn subequent recreation.

--- StateChange/app/src/main/java/com/ebookfrenzy/statechange/MainActivity.java
+++ StateChange/app/src/main/java/com/ebookfrenzy/statechange/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 //import android.util.Log to use Log class.
 
 public class MainActivity extends AppCompatActivity {
@@ -59,7 +60,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        Log.i(TAG,"onRestoreInstanceState");
+        Log.i(TAG,"onSaveInstanceState");
+
+        final EditText editText = findViewById(R.id.editText);
+        //obtain a reference to the view
+
+        //obtain the text that the editText view contains via the object's getText() method:
+        CharSequence userText = editText.getText();
+
+        //Save teh text using the Bundle object's putCharSequence method, passing through the key "savedText", and the userText object as arguments:
+        outState.putCharSequence("savedText",userText);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG, "onRestoreInstanceState");
+
+        final EditText editText = findViewById(R.id.editText);
+
+        CharSequence userText = savedInstanceState.getCharSequence("savedText");
+
+        editText.setText(userText);
     }
 
 
