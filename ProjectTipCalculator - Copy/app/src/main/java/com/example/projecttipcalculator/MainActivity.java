6034145ca59d89d38d9686d9947f7027dee25016
Luna Meng Jia <luna.jia6@gmail.com>
2021-03-15 23:23:07 -0400

    add cardDemo

--- ProjectTipCalculator - Copy/app/src/main/java/com/example/projecttipcalculator/MainActivity.java
+++ ProjectTipCalculator - Copy/app/src/main/java/com/example/projecttipcalculator/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.EditText; 
 import android.widget.TextView;
 
 import java.util.Locale;
